d86dd480a78df00c51ae15eb94e62835ab582919
Mike Amaral <mike.amaral@ping4.com>
2016-04-24 01:00:04 -0400

    Initial implementation of the scheduled broadcasts model/requests.

--- Source/Models/Scheduled Broadcast/LTVScheduledBroadcast.h
+++ Source/Models/Scheduled Broadcast/LTVScheduledBroadcast.h
@@ -7,7 +7,21 @@
 //
 
 #import "LTVObject.h"
+#import "LTVAPI.h"
 
 @interface LTVScheduledBroadcast : LTVObject
+
+@property (nonatomic, readonly) NSInteger broadcastID;
+@property (nonatomic, strong, readonly) NSString *title;
+@property (nonatomic, strong, readonly) NSString *livestream;
+@property (nonatomic, strong, readonly) NSString *codingCategory;
+@property (nonatomic, readonly) LTVDifficulty difficulty;
+@property (nonatomic, strong, readonly) NSString *startTimeString;
+@property (nonatomic, strong, readonly) NSString *startTimeOriginalTimezone;
+@property (nonatomic, strong, readonly) NSString *originalTimezone;
+@property (nonatomic, readonly) BOOL isFeatured;
+@property (nonatomic, readonly) BOOL isRecurring;
+
++ (void)getScheduledBroadcastsWithOffset:(NSInteger)offset limit:(NSInteger)limit handler:(void (^)(NSString *errorMessage, NSArray *broadcasts))handler;
 
 @end
--- Source/Models/Scheduled Broadcast/LTVScheduledBroadcast.m
+++ Source/Models/Scheduled Broadcast/LTVScheduledBroadcast.m
@@ -7,7 +7,60 @@
 //
 
 #import "LTVScheduledBroadcast.h"
+#import "LTVRequestManager.h"
 
 @implementation LTVScheduledBroadcast
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super initWithDictionary:dictionary];
+
+    if (self == nil) {
+        return nil;
+    }
+
+    _broadcastID = [dictionary[@"id"] integerValue];
+    _title = dictionary[@"title"];
+    _livestream = dictionary[@"livestream"];
+    _codingCategory = dictionary[@"coding_category"];
+    _difficulty = [self difficultyFromString:dictionary[@"difficulty"]];
+    _startTimeString = dictionary[@"start_time"];
+    _startTimeOriginalTimezone = dictionary[@"start_time_original_timezone"];
+    _originalTimezone = dictionary[@"original_timezone"];
+    _isFeatured = [dictionary[@"is_featured"] boolValue];
+    _isRecurring = [dictionary[@"is_recurring"] boolValue];
+
+    return self;
+}
+
+
+#pragma mark - Requests
+
++ (void)getScheduledBroadcastsWithOffset:(NSInteger)offset limit:(NSInteger)limit handler:(void (^)(NSString *errorMessage, NSArray *broadcasts))handler {
+    NSParameterAssert(handler);
+
+    NSString *route = @"scheduledbroadcasts";
+    NSDictionary *params = @{@"limit": @(limit), @"offset": @(offset)};
+
+    [[LTVRequestManager sharedInstance] sendGETRequestToRoute:route params:params handler:^(NSString *errorMessage, id responseObject) {
+        if (errorMessage) {
+            handler(errorMessage, nil);
+            return;
+        }
+
+        NSDictionary *responseDict = (NSDictionary *)responseObject;
+        NSArray *resultsArray = responseDict[@"results"];
+        NSMutableArray *broadcasts = [NSMutableArray arrayWithCapacity:resultsArray.count];
+
+        for (NSDictionary *resultDict in resultsArray) {
+            LTVScheduledBroadcast *broadcast = [[LTVScheduledBroadcast alloc] initWithDictionary:resultDict];
+
+            if (broadcast) {
+                [broadcasts addObject:broadcast];
+            }
+        }
+
+        handler(nil, broadcasts);
+    }];
+}
 
 @end
